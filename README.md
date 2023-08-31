aaffb921ed15304dc90fb747c9d9d0d3a974b0e7
Lucas Sta Maria <lucas@priime.dev>
2023-08-31 17:59:46 -0400

    chore: setup base project
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

---
+++ README.md
@@ -0,0 +1,4 @@
+# hash-star
+
+A package that provides additional functions for interacting with
+hash tables, specifically nested hash tables.
---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define collection "hash-star")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/hash-star.scrbl" ())))
+(define pkg-desc "Utility functions for traversing hashtable structures")
+(define version "0.1")
+(define pkg-authors '(priime0))
+(define license 'MIT)
